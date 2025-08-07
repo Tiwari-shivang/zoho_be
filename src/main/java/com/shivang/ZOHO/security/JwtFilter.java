48fe5cda4d5d1f988cecde8b8459180049610665
Shivang Tiwari <shivangtiwari@Shivangs-MacBook-Air.local>
2025-08-07 17:08:16 +0530

    Auth setup completed

---
+++ src/main/java/com/shivang/ZOHO/security/JwtFilter.java
@@ -0,0 +1,46 @@
+package com.shivang.ZOHO.security;
+
+import com.shivang.ZOHO.services.impl.UserService_Impl;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+    private UserService_Impl service;
+
+    @Autowired
+    public JwtFilter(JwtUtils jwtUtils, UserService_Impl service){
+        this.jwtUtils = jwtUtils;
+        this.service = service;
+    }
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if(request.getServletPath().startsWith("/api/auth")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String authHeader = request.getHeader("Authorization");
+        String token = authHeader.substring(7);
+        String email = jwtUtils.extractSubject(token);
+        UserDetails user = service.loadUserByUsername(email);
+        if(user != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            if(jwtUtils.validateToken(token, user)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user,null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+                filterChain.doFilter(request, response);
+            }
+        }
+    }
+}
